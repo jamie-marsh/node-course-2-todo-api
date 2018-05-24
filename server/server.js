e6991093da749fb39e79597925a1b40e5d4eae07
jamie marsh <jmarsh@Panoramas-iMac-3.local>
2018-05-24 15:41:23 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +29,25 @@ app.get('/todos', (req, res) => {
        res.send({todos}); 
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+//GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else {
+            return res.status(404).send();
+        }
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
