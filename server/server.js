eea66fdfa0c5d5befaf55154fd67d706b1249f5e
jamie marsh <jmarsh@Panoramas-iMac-3.local>
2018-05-30 15:37:45 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -117,4 +117,17 @@ app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        console.log('Error:', e);
+        res.status(400).send();
+    });
+});
+
 module.exports = {app};
